33c563f9dfd7026e938401e80c6bff0b40b9c8e0
eloco <22910374+Eloco@users.noreply.github.com>
2023-02-23 11:31:07 +0800

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   LICENSE 	new file:   README.md 	new file:   assets/icon.png 	new file:   babel.config.json 	new file:   manifest.xml 	new file:   manifest.xml.bak 	new file:   package-lock.json 	new file:   package.json 	new file:   src/commands/commands.html 	new file:   src/commands/commands.js 	new file:   src/taskpane/taskpane.css 	new file:   src/taskpane/taskpane.html 	new file:   src/taskpane/taskpane.js 	new file:   tsconfig.json 	new file:   webpack.config.js

---
+++ README.md
@@ -0,0 +1,1 @@
+# Excel-DPA-test
---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("btn").addEventListener("click",writeData);
+  }
+});
+
+export async function writeData() {
+    // Excel.run((context) => {
+    //     const ws= context.workbook.worksheets.getActiveWorksheet();
+    //     const range = ws.getRange("A1:A2")
+    //     range.values = [[44],[77]]
+    //     return context.sync()
+    // });
+    Excel.run(async (context) => {
+        const fileUrl = "http://127.0.0.1:8088/myfile.xlsx";
+        const fileName = "myfile.xlsx";
+        const workbook = await context.workbook;
+        const sheets = workbook.worksheets;
+        // 从 URL 下载文件
+        const response = await fetch(fileUrl,{
+                                     mode: 'cors',
+                                     headers: {'Access-Control-Allow-Origin': "*"},
+                                    });
+        const data = await response.arrayBuffer();
+        // 创建一个 Base64 字符串以在 Excel 中加载文件
+        const base64Data = btoa(String.fromCharCode.apply(null, new Uint8Array(data)));
+        // 将文件加载到 Excel 中
+        const worksheet = sheets.getItem(0);
+        worksheet.activate();
+        worksheet.getRange("A1").select();
+        worksheet.paste(base64Data, "xlsx");
+        // 重命名工作簿
+        workbook.name = fileName;
+      await context.sync();
+      }).catch((error) => {
+        console.error(error);
+      });
+}
